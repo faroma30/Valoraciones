78506fc2145e0255d44a105b7b967071dbb0c221
faroma30 <fabian.rodriguez@verisure.es>
2026-09-21 15:18:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'valoraciones-v1-20260921-1518';
-const ASSETS = ['./', './index.html','./jspdf.bundle.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'valoraciones-v1-inline-20260921-1526';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
